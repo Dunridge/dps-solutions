a9b73c1feb6b5753d3594c38e58aa8b3de07f0dd
Dunridge <Polinka7max@gmail.com>
2022-11-06 13:49:10 +0200

    develop. fixed import

--- src/main/java/com/max/production/task3/VisitorSolution.java
+++ src/main/java/com/max/production/task3/VisitorSolution.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class VisitorSolution {
